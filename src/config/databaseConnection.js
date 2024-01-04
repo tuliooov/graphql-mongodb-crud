92e34fc6ce13f5d163e5811a1fbc82ae988d955d
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2024-01-04 12:42:48 -0300

    fix env

--- src/config/databaseConnection.js
+++ src/config/databaseConnection.js
@@ -1,19 +1,16 @@
 const MongoClient = require('mongodb').MongoClient;
-
 let mongoDB;
 
 const setupDB = async() => {
-  const uri = 'mongodb+srv://marcotuliovaleriano2014:tulio1207@cluster0.o0dhq6m.mongodb.net/?retryWrites=true&w=majority' 
-
   mongoDB = await MongoClient.connect(
-    uri,
+    process.env.MOGNGO_CONNECTION_STRING,
     { useNewUrlParser: true, useUnifiedTopology: true },
   )
 
 };
 
 const getDB = () => {
-   return mongoDB.db("GraphQL_DB_TEST")
+  return mongoDB.db("GraphQL_DB_TEST")
 };
 
 module.exports = { setupDB, getDB };
